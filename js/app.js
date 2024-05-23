bef5f932073397067c7a434f8eb9bb44ded2c4a7
Emanuel Morales <101947718+Kuro-98@users.noreply.github.com>
2024-05-23 17:25:53 -0600

    validaciones anadidas y alerts

--- js/app.js
+++ js/app.js
@@ -6,13 +6,58 @@ const gastoListado = document.querySelector('#gastos ul');
 eventListeners();
 function eventListeners() {
 	document.addEventListener('DOMContentLoaded', preguntarPresupuesto);
+	formulario.addEventListener('submit', agregarGasto);
 }
 //clases
 
+class Presupuesto {
+	constructor(presupuesto) {
+		this.presupuesto = Number(presupuesto);
+		this.restante = Number(presupuesto);
+		this.gastos = [];
+	}
+
+	nuevoGasto(gasto) {
+		this.gastos = [...this.gastos, gasto];
+		console.log(this.gastos);
+	}
+}
+
+class UI {
+	insertarPresupuesto(cantidad) {
+		const { presupuesto, restante } = cantidad;
+		document.querySelector('#total').textContent = presupuesto;
+		document.querySelector('#restante').textContent = restante;
+	}
+
+	imprimirAlerta(mensaje, tipo) {
+		const div = document.createElement('div');
+		div.textContent = mensaje;
+		div.classList.add('text-center', 'alert');
+
+		if (tipo === 'error') {
+			div.classList.add('alert-danger');
+		} else {
+			div.classList.add('alert-success');
+		}
+		formulario.insertAdjacentElement('beforebegin', div);
+		setTimeout(() => {
+			div.remove();
+		}, 3000);
+	}
+
+	agregarGastoListado(gastos) {
+		gastos.forEach((gasto) => {
+			console.log(gasto);
+		});
+	}
+}
+const ui = new UI();
+let presupuesto;
 //funciones
 function preguntarPresupuesto() {
-	const presupuestoUsuario = Number(prompt('Cuanta lana tienes??'));
-	console.log(presupuestoUsuario);
+	const presupuestoUsuario = prompt('Cuanta lana tienes??');
+
 	if (
 		presupuestoUsuario === '' ||
 		presupuestoUsuario === null ||
@@ -21,4 +66,29 @@ function preguntarPresupuesto() {
 	) {
 		window.location.reload();
 	}
+
+	//presupuesto valido
+	presupuesto = new Presupuesto(presupuestoUsuario);
+	ui.insertarPresupuesto(presupuesto);
+}
+
+function agregarGasto(e) {
+	e.preventDefault();
+	const nombre = document.querySelector('#gasto').value;
+	const cantidad = Number(document.querySelector('#cantidad').value);
+
+	if (nombre === '' || cantidad === '') {
+		ui.imprimirAlerta('ambos campos son obligatorios', 'error');
+		return;
+	} else if (cantidad <= 0 || isNaN(cantidad)) {
+		ui.imprimirAlerta('cantidad no valida', 'error');
+		return;
+	}
+
+	const gasto = { nombre, cantidad, id: Date.now() };
+	presupuesto.nuevoGasto(gasto);
+	ui.imprimirAlerta('Gasto agregado correctamente');
+	const { gastos } = presupuesto;
+	ui.agregarGastoListado(gastos);
+	formulario.reset();
 }
